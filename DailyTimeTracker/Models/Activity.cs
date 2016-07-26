1878d105137bcda1961f4b6df10e9c5daa60d33f
Sherry Ummen <sherry.ummen@gmail.com>
2016-07-26 23:27:22 +0300

    Activity Model

---
+++ DailyTimeTracker/Models/Activity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyTimeTracker.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public ActivityCategory Category { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DailyTimeTracker/Models/ActivityCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyTimeTracker.Models
+{
+    public class ActivityCategory
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+    }
+}
