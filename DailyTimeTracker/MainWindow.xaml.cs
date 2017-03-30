fd4a8c3f746e5fb5412941c75b11a3e14bea746b
Sherry Ummen <sherry.ummen@gmail.com>
2017-03-30 23:53:30 +0300

    Idle logic (WIP)

--- DailyTimeTracker/MainWindow.xaml.cs
+++ DailyTimeTracker/MainWindow.xaml.cs
@@ -16,12 +16,22 @@ namespace DailyTimeTracker {
         private void MainWindow_Loaded(object sender, RoutedEventArgs e) {
             IdleTimeNotifier.StartNotifier(5);
             IdleTimeNotifier.Idle += () => Dispatcher.BeginInvoke(new Action(() => {
-                //Box.Text += "Idle now ";
+
+                Application.Current.MainWindow.Activate();
+                Application.Current.MainWindow.Topmost = true;
+                Application.Current.MainWindow.ShowActivated = false;
+                if (Application.Current.MainWindow.WindowState == WindowState.Minimized)
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                /*
+                 On idle record the time of idle
+                 when user comes back give him the option to contiue what he was doing earlier or start new task
+                 */
+
             }));
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e){
-           
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+
         }
     }
 }
