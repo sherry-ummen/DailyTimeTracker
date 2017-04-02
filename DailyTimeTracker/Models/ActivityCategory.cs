7360e7d9e801ede2f7b6235875346ad0789db6ed
Sherry Ummen <sherry.ummen@gmail.com>
2017-04-02 14:22:53 +0300

    extracting activitycontrol to usercontrol

--- DailyTimeTracker/Models/ActivityCategory.cs
+++ DailyTimeTracker/Models/ActivityCategory.cs
@@ -2,6 +2,6 @@
     public class ActivityCategory
     {
         public int Id { get; set; }
-        public string Category { get; set; }
+        public string Category { get; set; } //TODO: Change this to be Enum
     }
 }
